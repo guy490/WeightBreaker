b606b0632432f86d07ea8cc08fc0a08593793edb
guy490 <guy8824@gmail.com>
2019-08-29 21:25:50 +0200

    Start menu and levels panel

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MenuController : MonoBehaviour
+{
+    [SerializeField]
+    private Animator levelPanel;
+    [SerializeField]
+    private Animator menuPanel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void LevelMenuIn()
+    {
+        menuPanel.Play("MenuOut");
+        levelPanel.Play("LevelsPanelIn");
+    }
+    public void StartLevel(string level)
+    {
+        SceneManager.LoadScene(level);
+    }
+}
--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class UIController : MonoBehaviour
 {
+    
     [SerializeField]
     private GameObject stackView;
     private Animator stackViewAnimator;
